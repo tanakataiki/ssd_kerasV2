03d435abe7e437eb04f313cd3465619f3857bdc1
tanakataiki <ttanaka@iis.u-tokyo.ac.jp>
2018-04-25 20:07:52 +0900

    Add

---
+++ testing_utils/videotest_example.py
@@ -0,0 +1,25 @@
+import keras
+import pickle
+from videotest import VideoTest
+
+import sys
+sys.path.append("..")
+from model.ssd300MobileNet import SSD
+
+input_shape = (300,300,3)
+
+# Change this if you run with other classes than VOC
+class_names = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"];
+NUM_CLASSES = len(class_names)
+
+model = SSD(input_shape, num_classes=NUM_CLASSES)
+
+# Change this path if you want to use your own trained weights
+model.summary()
+model.load_weights('./../../../WeightsNew/MobileNetSSD300_1weights_voc_2007_class20.hdf5')
+        
+vid_test = VideoTest(class_names, model, input_shape)
+
+# To test on webcam 0, remove the parameter (or change it to another number
+# to test on that webcam)
+vid_test.run('./../../../VIDEO_DEV/soccer.mp4')
